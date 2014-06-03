cbf984a5e248c0a5882eeb7093e6403a67948c11
Louis BARRANQUEIRO <louis.barranqueiro@gmail.com>
2014-06-03 19:46:05 +0200

    Création d'un objet "FormatExamen"

---
+++ src/beans/FormatExamen.java
@@ -0,0 +1,37 @@
+package beans;
+
+public class FormatExamen
+{
+	private Long id;
+	private String nom;
+	
+	public FormatExamen() 
+	{
+		this.id = null;
+		this.nom = null;
+	}
+	
+	public FormatExamen(FormatExamen format) 
+	{
+		this.setId(format.getId());
+		this.setNom(format.getNom());
+	}
+	
+	public Long getId() 
+	{
+		return id;
+	}
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public String getNom()
+	{
+		return nom;
+	}
+	public void setNom(String nom) 
+	{
+		this.nom = nom;
+	}
+ 
+}
