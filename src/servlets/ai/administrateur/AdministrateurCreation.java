de309932978c3cbf87ca8f23a1d2d16ee7cd8b50
Louis BARRANQUEIRO <louis.barranqueiro@gmail.com>
2014-06-22 15:52:13 +0200

    Optimisation des imports dans les servlets de gestion des administrateurs

--- src/servlets/ai/administrateur/AdministrateurCreation.java
+++ src/servlets/ai/administrateur/AdministrateurCreation.java
@@ -6,7 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import dao.DAOFactory;
 import dao.AdministrateurDao;
 import forms.AdministrateurForm;
