515b8c17a1b4d0ae0b551d4ab35dc32fd45739d2
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-12 16:37:28 +0200

    Create task to javascript files

---
+++ src/main/tasks/config/concat.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+    grunt.config.set('concat', {
+        // Concat all javascript file into `script.js`
+        dev:  {
+            src:     [
+                'src/main/webapp/resources/_js/utils/*.js',
+                'src/main/webapp/resources/_js/components/*.js',
+                'src/main/webapp/resources/_js/beans/*.js'
+            ],
+            dest:    'src/main/webapp/resources/js/script.js',
+            options: {
+                separator: ';',
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+};
