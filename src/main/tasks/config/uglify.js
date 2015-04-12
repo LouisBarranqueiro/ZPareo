f7e19927051833863e03d60aecc23d7d19063c00
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-12 16:43:52 +0200

    Create task to minify javascript files

---
+++ src/main/tasks/config/uglify.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+    grunt.config.set('uglify', {
+        // Minify `script.js` file into `script.min.js`
+        dev: {
+            options: {
+                mangle: {
+                    except: [
+                        'jQuery'
+                    ]
+                }
+            },
+            files:   {
+                'src/main/webapp/resources/js/script.min.js': ['src/main/webapp/resources/js/script.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+};
