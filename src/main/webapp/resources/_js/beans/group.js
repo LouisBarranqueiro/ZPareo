a7ef2acaf9f62bda431f2dfebd21432e678de949
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-12 22:50:59 +0200

    Improve code quality of group javascript functions

--- src/main/webapp/resources/_js/beans/group.js
+++ src/main/webapp/resources/_js/beans/group.js
@@ -1,38 +1,38 @@
 /**
- * Creates a group
+ * Create a group
  */
 var createGroup = function() {
     $('#create-group').submit(function(event) {
         event.preventDefault();
         var name        = $('#create-group input[name=name]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
-        $.ajax(
-            {
-                type:    'POST',
-                url:     baseURL + '/zpareo/ai/groupe/creation',
-                data:    {
-                    name: name
-                },
-                error:   function() {
-                    alert("error !");
-                },
-                success: function(view) {
-                    if (view.search('<div id=\"main-wrap\"') > 0) {
-                        removeModalWindow();
-                        $('#main-wrap').replaceWith(sliceMainView(view));
-                    }
-                    else {
-                        modalWindow.html(view);
-                        initModalWindow(300);
-                    }
+        $.ajax({
+            type:    'POST',
+            url:     baseURL + url,
+            data:    {
+                name: name
+            },
+            error:   function() {
+                alert("error!");
+            },
+            success: function(view) {
+                if (view.search('<div id=\"main-wrap\"') > 0) {
+                    removeModalWindow();
+                    $('#main-wrap').replaceWith(sliceMainView(view));
                 }
-            });
+                else {
+                    modalWindow.html(view);
+                    initModalWindow(300);
+                }
+            }
+        });
     });
 };
 
 /**
- * Edits a group
+ * Edit a group
  */
 var editGroup = function() {
     $('#edit-group').submit(function(event) {
@@ -40,61 +40,62 @@ var editGroup = function() {
         var id          = $('#edit-group input[name=id]').val();
         var name        = $('#edit-group input[name=name]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
-        $.ajax(
-            {
-                type:    'POST',
-                url:     baseURL + '/zpareo/ai/groupe/edition',
-                data:    {
-                    id:   id,
-                    name: name
-                },
-                error:   function() {
-                    alert("erreur !");
-                },
-                success: function(view) {
-                    if (view.search('<div id=\"main-wrap\"') > 0) {
-                        removeModalWindow();
-                        $('#main-wrap').replaceWith(sliceMainView(view));
-                    }
-                    else {
-                        modalWindow.html(view);
-                        initModalWindow(300);
-                    }
+        alert(baseURL + url);
+        $.ajax({
+            type:    'POST',
+            url:     baseURL + url,
+            data:    {
+                id:   id,
+                name: name
+            },
+            error:   function() {
+                alert("error!");
+            },
+            success: function(view) {
+                if (view.search('<div id=\"main-wrap\"') > 0) {
+                    removeModalWindow();
+                    $('#main-wrap').replaceWith(sliceMainView(view));
                 }
-            });
+                else {
+                    modalWindow.html(view);
+                    initModalWindow(300);
+                }
+            }
+        });
     });
 };
 
 /**
- * Deletes a group
+ * Delete a group
  */
 var deleteGroup = function() {
     $('#delete-group').submit(function(event) {
         event.preventDefault();
         var id          = $('#delete-group input[name=id]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
-        $.ajax(
-            {
-                type:    "POST",
-                url:     "http://localhost:8080/zpareo/ai/groupe/suppression",
-                data:    {
-                    id: id
-                },
-                error:   function() {
-                    alert("erreur !");
-                },
-                success: function(view) {
-                    if (view.search('<div id=\"main-wrap\"') > 0) {
-                        removeModalWindow();
-                        $('#main-wrap').replaceWith(sliceMainView(view));
-                    }
-                    else {
-                        modalWindow.html(view);
-                        initModalWindow(300);
-                    }
+        $.ajax({
+            type:    "POST",
+            url:     baseURL + url,
+            data:    {
+                id: id
+            },
+            error:   function() {
+                alert("error!");
+            },
+            success: function(view) {
+                if (view.search('<div id=\"main-wrap\"') > 0) {
+                    removeModalWindow();
+                    $('#main-wrap').replaceWith(sliceMainView(view));
                 }
-            });
+                else {
+                    modalWindow.html(view);
+                    initModalWindow(300);
+                }
+            }
+        });
     });
 };
