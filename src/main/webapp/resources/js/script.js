76a3996d12920ea990e00eb96fd6ac8637305b85
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-13 19:11:18 +0200

    Improve code style and comments

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -389,7 +389,7 @@ var deleteGroup = function() {
     });
 };
 ;/**
- * Creates a student
+ * Create a student
  */
 var createStudent = function() {
     $('#create-student').submit(function(event) {
@@ -399,37 +399,36 @@ var createStudent = function() {
         var emailAddress = $('#create-student input[name=emailAddress]').val();
         var group        = $('#create-student select[name=group]').val();
         var modalWindow  = $('#modal');
+        var url          = $(this).attr('action');
 
-        $.ajax(
-            {
-                type:    'POST',
-                url:     baseURL + '/zpareo/ai/etudiant/creation',
-                data:    {
-                    lastName:     lastName,
-                    firstName:    firstName,
-                    emailAddress: emailAddress,
-                    group:        group
-                },
-                error:   function() {
-                    alert('error !');
-                },
-                success: function(view) {
-                    if (view.search('<div id=\"main-wrap\"') > 0) {
-                        removeModalWindow();
-                        $('#main-wrap').replaceWith(sliceMainView(view));
-                    }
-                    else {
-                        modalWindow.html(view);
-                        initModalWindow(300);
-
-                    }
+        $.ajax({
+            type:    'POST',
+            url:     baseURL + url,
+            data:    {
+                lastName:     lastName,
+                firstName:    firstName,
+                emailAddress: emailAddress,
+                group:        group
+            },
+            error:   function() {
+                alert('error !');
+            },
+            success: function(view) {
+                if (view.search('<div id=\"main-wrap\"') > 0) {
+                    removeModalWindow();
+                    $('#main-wrap').replaceWith(sliceMainView(view));
                 }
-            });
+                else {
+                    modalWindow.html(view);
+                    initModalWindow(300);
+                }
+            }
+        });
     });
 };
 
 /**
- * Edits a student
+ * Edit a student
  */
 var editStudent = function() {
     $('#edit-student').submit(function(event) {
@@ -440,48 +439,48 @@ var editStudent = function() {
         var emailAddress = $('#edit-student input[name=emailAddress]').val();
         var group        = $('#edit-student select[name=group]').val();
         var modalWindow  = $('#modal');
+        var url          = $(this).attr('action');
 
-        $.ajax(
-            {
-                type:    'POST',
-                url:     baseURL + '/zpareo/ai/etudiant/edition',
-                data:    {
-                    id:           id,
-                    lastName:     lastName,
-                    firstName:    firstName,
-                    emailAddress: emailAddress,
-                    group:        group
-                },
-                error:   function() {
-                    alert("error !");
-                },
-                success: function(view) {
-                    if (view.search('<div id=\"main-wrap\"') > 0) {
-                        removeModalWindow();
-                        $('#main-wrap').replaceWith(sliceMainView(view));
-                    }
-                    else {
-                        modalWindow.html(view);
-                        initModalWindow(300);
-
-                    }
+        $.ajax({
+            type:    'POST',
+            url:     baseURL + url,
+            data:    {
+                id:           id,
+                lastName:     lastName,
+                firstName:    firstName,
+                emailAddress: emailAddress,
+                group:        group
+            },
+            error:   function() {
+                alert("error !");
+            },
+            success: function(view) {
+                if (view.search('<div id=\"main-wrap\"') > 0) {
+                    removeModalWindow();
+                    $('#main-wrap').replaceWith(sliceMainView(view));
                 }
-            });
+                else {
+                    modalWindow.html(view);
+                    initModalWindow(300);
+                }
+            }
+        });
     });
 };
 
 /**
- * Deletes a student
+ * Delete a student
  */
 var deleteStudent = function() {
     $('#delete-student').submit(function(event) {
         event.preventDefault();
         var id          = $('#delete-student input[name=id]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
         $.ajax({
             type:    'POST',
-            url:     baseURL + '/zpareo/ai/etudiant/suppression',
+            url:     baseURL + url,
             data:    {
                 id: id
             },
@@ -496,7 +495,6 @@ var deleteStudent = function() {
                 else {
                     modalWindow.html(view);
                     initModalWindow(300);
-
                 }
             }
         });
@@ -504,42 +502,43 @@ var deleteStudent = function() {
 };
 
 /**
- * Resets a user password
+ * Reset a user password
  */
-var resetPassStudent = function() {
-    $('#reinit-pass-student').submit(function(event) {
+var resetPasswordStudent = function() {
+    $('#reset-pass-student').submit(function(event) {
         event.preventDefault();
-        var id = $('#reinit-pass-student input[name=id]').val();
+        var id  = $('#reinit-pass-student input[name=id]').val();
+        var url = $(this).attr('action');
 
-        $.ajax(
-            {
-                type:    'POST',
-                url:     baseURL + '/zpareo/ai/etudiant/reinit-mot-de-passe',
-                data:    {
-                    id: id
-                },
-                error:   function() {
-                    alert("error !");
-                },
-                success: function(data) {
-                    removeModalWindow();
-                }
-            });
+        $.ajax({
+            type:    'POST',
+            url:     baseURL + url,
+            data:    {
+                id: id
+            },
+            error:   function() {
+                alert("error !");
+            },
+            success: function(data) {
+                removeModalWindow();
+            }
+        });
     });
 };
 ;/**
- * Creates a matter
+ * Creates a subject
  */
-var createMatter = function() {
-    $('#create-matter').submit(function(event) {
+var createSubject = function() {
+    $('#create-subject').submit(function(event) {
         event.preventDefault();
-        var name        = $('#create-matter input[name=name]').val();
+        var name        = $('#create-subject input[name=name]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
         $.ajax(
             {
                 type:    'POST',
-                url:     baseURL + '/zpareo/ai/matiere/creation',
+                url:     baseURL + url,
                 data:    {
                     name: name
                 },
@@ -554,7 +553,6 @@ var createMatter = function() {
                     else {
                         modalWindow.html(view);
                         initModalWindow(300);
-
                     }
                 }
             });
@@ -562,19 +560,20 @@ var createMatter = function() {
 };
 
 /**
- * Edits a matter
+ * Edits a subject
  */
-var editMatter = function() {
-    $('#edit-matter').submit(function(event) {
+var editSubject = function() {
+    $('#edit-subject').submit(function(event) {
         event.preventDefault();
-        var id          = $('#edit-matter input[name=id]').val();
-        var name        = $('#edit-matter input[name=name]').val();
+        var id          = $('#edit-subject input[name=id]').val();
+        var name        = $('#edit-subject input[name=name]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
         $.ajax(
             {
                 type:    'POST',
-                url:     baseURL + '/zpareo/ai/matiere/edition',
+                url:     baseURL + url,
                 data:    {
                     id:   id,
                     name: name
@@ -597,18 +596,19 @@ var editMatter = function() {
 };
 
 /**
- * Deletes a matter
+ * Deletes a subject
  */
-var deleteMatter = function() {
-    $('#delete-matter').submit(function(event) {
+var deleteSubject = function() {
+    $('#delete-subject').submit(function(event) {
         event.preventDefault();
-        var id          = $('#delete-matter input[name=id]').val();
+        var id          = $('#delete-subject input[name=id]').val();
         var modalWindow = $('#modal');
+        var url         = $(this).attr('action');
 
         $.ajax(
             {
                 type:    'POST',
-                url:     baseURL + '/zpareo/ai/matiere/suppression',
+                url:     baseURL + url,
                 data:    {
                     id: id
                 },
@@ -623,7 +623,6 @@ var deleteMatter = function() {
                     else {
                         modalWindow.html(view);
                         initModalWindow('auto');
-
                     }
                 }
             });
