6ce0554346dcdbf8e3fc9c932568e9a75e83b44d
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-13 11:35:26 +0200

    Improve code quality of beans

--- src/main/java/beans/Administrator.java
+++ src/main/java/beans/Administrator.java
@@ -1,36 +1,29 @@
 package beans;
 
-public class Administrator extends User implements Comparable<Administrator>
-{
-	/**
-	 * Constructor
-	 * 
-	 */
-	public Administrator()
-	{
-		super();
-	}
-	/**
-	 * Constructor
-	 * 
-	 * @param administrator 
-	 */
-	public Administrator(Administrator administrator)
-	{
-		super(administrator);
-	}
-	
-	/**
-	 * Compares two administrators's id
-	 * 
-	 * @param administrator2 
-	 */
-	@Override
-	public int compareTo(Administrator administrator2) 
-	{
-		int compId = administrator2.getId().compareTo(this.getId());
-        
-		return ((compId != 0) ? compId : 0);
-	}
+public class Administrator extends User implements Comparable<Administrator> {
+    /**
+     * Constructor
+     */
+    public Administrator() {
+        super();
+    }
+
+    /**
+     * Constructor
+     * @param administrator
+     */
+    public Administrator(Administrator administrator) {
+        super(administrator);
+    }
+
+    /**
+     * Compares two administrators's id
+     * @param administrator2
+     */
+    public int compareTo(Administrator administrator2) {
+        int compId = administrator2.getId().compareTo(this.getId());
+
+        return ((compId != 0) ? compId : 0);
+    }
 
 }
--- src/main/java/beans/Teacher.java
+++ src/main/java/beans/Teacher.java
@@ -2,83 +2,69 @@ package beans;
 
 import java.util.Set;
 
-public class Teacher extends User implements Comparable<Teacher>
-{
-	private Set<Matter> matters;
-	private Set<Group> groups;
-	
-	/**
-	 * Constructor
-	 */
-	public Teacher()
-	{
-		super();
-		this.matters = null;
-		this.groups = null;
-	}
-	/**
-	 * Constructor
-	 * 
-	 * @param teacher 
-	 */
-	public Teacher(Teacher teacher)
-	{
-		super(teacher);
-		this.matters = teacher.getMatters();
-		this.groups = teacher.getGroups();
-	}
-	
-	/**
-	 * Returns teacher matters
-	 * 
-	 * @return matters
-	 */
-	public Set<Matter> getMatters()
-	{
-		return matters;
-	}
-	
-	/**
-	 * Sets teacher matters
-	 * 
-	 * @param matters 
-	 */
-	public void setMatters(Set<Matter> matters) 
-	{
-		this.matters = matters;
-	}
-	
-	/**
-	 * Returns teacher groups
-	 * 
-	 * @return groups 
-	 */
-	public Set<Group> getGroups() 
-	{
-		return groups;
-	}
-	
-	/**
-	 * Sets teacher groups
-	 * 
-	 * @param groups 
-	 */
-	public void setGroups(Set<Group> groups) 
-	{
-		this.groups = groups;
-	}
-	
-	/**
-	 * Compares two teacher id
-	 * 
-	 * @param teacher2 
-	 */
-	@Override
-	public int compareTo(Teacher teacher2) 
-	{
+public class Teacher extends User implements Comparable<Teacher> {
+    private Set<Subject> subjects;
+    private Set<Group>   groups;
+
+    /**
+     * Constructor
+     */
+    public Teacher() {
+        super();
+        this.subjects = null;
+        this.groups = null;
+    }
+
+    /**
+     * Constructor
+     * @param teacher
+     */
+    public Teacher(Teacher teacher) {
+        super(teacher);
+        this.subjects = teacher.getSubjects();
+        this.groups = teacher.getGroups();
+    }
+
+    /**
+     * Returns teacher subjects
+     * @return subjects
+     */
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+    /**
+     * Sets teacher subjects
+     * @param subjects
+     */
+    public void setSubjects(Set<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    /**
+     * Returns teacher groups
+     * @return groups
+     */
+    public Set<Group> getGroups() {
+        return groups;
+    }
+
+    /**
+     * Sets teacher groups
+     * @param groups
+     */
+    public void setGroups(Set<Group> groups) {
+        this.groups = groups;
+    }
+
+    /**
+     * Compares two teacher id
+     * @param teacher2
+     */
+    public int compareTo(Teacher teacher2) {
         int compId = teacher2.getId().compareTo(this.getId());
 
         return ((compId != 0) ? compId : 0);
-	}
-	
+    }
+
 }
