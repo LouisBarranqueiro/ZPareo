9a4d57ec93b65be86febb929df460acd34f6f81c
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-13 11:40:59 +0200

    Improve code qualify of dao objects

--- src/main/java/dao/MatterDao.java
+++ src/main/java/dao/MatterDao.java
@@ -1,56 +1,49 @@
- package dao;
- 
+package dao;
+
 import java.util.Set;
+
 import beans.Subject;
 
- public interface MatterDao 
- {
- 	/**
-      * Creates a subject into database
-      * 
-      * @param subject
-      * @throws DAOException
-      */
- 	void create(Subject subject) throws DAOException;
-	
-	/**
-     * Searches one or more matters into database
-     * 
+public interface MatterDao {
+    /**
+     * Creates a subject into database
      * @param subject
      * @throws DAOException
      */
-	Set<Subject> search(Subject subject) throws DAOException;
-	
+    void create(Subject subject) throws DAOException;
 
- 	/**
- 	 * Edits a subject into database
- 	 * 
- 	 * @param subject
- 	 * @throws DAOException
- 	 */
- 	void edit(Subject subject) throws DAOException;
- 	
- 	/**
-	 * Checks the existance of a subject into database
-	 * 
-	 * @param subject
-	 * @throws DAOException
-	 */
-	int check(Subject subject) throws DAOException;
-	
-	/**
- 	 * Returns a subject into database
- 	 * 
- 	 * @param subject
- 	 * @throws DAOException
- 	 */
- 	Subject get(Subject subject) throws DAOException;
- 	
- 	/**
- 	 * Deletes a subject into database
- 	 * 
- 	 * @param subject
- 	 * @throws DAOException
- 	 */
- 	void delete(Subject subject) throws DAOException;
- }
+    /**
+     * Searches one or more matters into database
+     * @param subject
+     * @throws DAOException
+     */
+    Set<Subject> search(Subject subject) throws DAOException;
+
+    /**
+     * Edits a subject into database
+     * @param subject
+     * @throws DAOException
+     */
+    void edit(Subject subject) throws DAOException;
+
+    /**
+     * Checks the existance of a subject into database
+     * @param subject
+     * @throws DAOException
+     */
+    int check(Subject subject) throws DAOException;
+
+    /**
+     * Returns a subject into database
+     * @param subject
+     * @throws DAOException
+     */
+    Subject get(Subject subject) throws DAOException;
+
+    /**
+     * Deletes a subject into database
+     * @param subject
+     * @throws DAOException
+     */
+    void delete(Subject subject) throws DAOException;
+}
--- src/main/java/dao/TestDao.java
+++ src/main/java/dao/TestDao.java
@@ -4,47 +4,41 @@ import java.util.Set;
 
 import beans.Test;
 
-public interface TestDao 
-{
-	/**
+public interface TestDao {
+    /**
      * Creates a test into database
-     * 
      * @param test
      * @throws DAOException
      */
-	void create(Test test) throws DAOException;
-	
-	/**
+    void create(Test test) throws DAOException;
+
+    /**
      * Searches one or more tests into database
-     * 
      * @param test
      * @return Set<Test>
      * @throws DAOException
      */
-	Set<Test> search(Test test) throws DAOException;
-	
-	/**
-	 * Edits a test into database
-	 * 
-	 * @param test
-	 * @throws DAOException
-	 */
-	void edit(Test test) throws DAOException;
-	
-	/**
-	 * Returns a test into database
-	 * 
-	 * @param test
-	 * @return Test
-	 * @throws DAOException
-	 */
-	Test get(Test test) throws DAOException;
+    Set<Test> search(Test test) throws DAOException;
 
-	/**
-	 * Deletes a test into database
-	 * 
-	 * @param test
-	 * @throws DAOException
-	 */
-	void delete(Test test) throws DAOException;
+    /**
+     * Edits a test into database
+     * @param test
+     * @throws DAOException
+     */
+    void edit(Test test) throws DAOException;
+
+    /**
+     * Returns a test into database
+     * @param test
+     * @return Test
+     * @throws DAOException
+     */
+    Test get(Test test) throws DAOException;
+
+    /**
+     * Deletes a test into database
+     * @param test
+     * @throws DAOException
+     */
+    void delete(Test test) throws DAOException;
 }
