c2a3fb39b61f181348c5873297816e7f152f55a5
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-14 12:12:50 +0200

    Improve code quality and rename some other variables

--- src/main/java/servlets/pi/test/Test.java
+++ src/main/java/servlets/pi/test/Test.java
@@ -7,44 +7,38 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import dao.DAOFactory;
 import dao.TestDao;
 import forms.TestForm;
 
-@SuppressWarnings("serial")
-@WebServlet("/pi/examen")
-public class Test extends HttpServlet 
-{	
-	private static final String CONF_DAO_FACTORY = "daofactory";
-	private static final String TESTS            = "tests";
-	private static final String NUMB_TESTS       = "numbTests";
-	private static final String TEST_FORM        = "testForm";
-    private static final String VIEW             = "/WEB-INF/pi/test/index.jsp";
-	private TestDao testDao;
-	
-	public Test() 
-    {
+@WebServlet("/ti/test")
+public class Test extends HttpServlet {
+    private static final String CONF_DAO_FACTORY = "daofactory";
+    private static final String TESTS            = "tests";
+    private static final String NUMB_TESTS       = "numbTests";
+    private static final String TEST_FORM        = "testForm";
+    private static final String VIEW             = "/WEB-INF/pi/test/index.xhtml";
+    private TestDao testDao;
+
+    public Test() {
         super();
     }
-	
-	public void init() throws ServletException 
-    {
+
+    public void init() throws ServletException {
         this.testDao = ((DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY)).getTestDao();
     }
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		TestForm testForm     = new TestForm(testDao);
-		Set<beans.Test> tests = testForm.search(request);
-		
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TestForm        testForm = new TestForm(testDao);
+        Set<beans.Test> tests    = testForm.search(request);
+
         request.setAttribute(TEST_FORM, testForm);
         request.setAttribute(TESTS, tests);
         request.setAttribute(NUMB_TESTS, tests.size());
-		this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
-	}
+        this.getServletContext().getRequestDispatcher(VIEW).forward(request, response);
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 }
