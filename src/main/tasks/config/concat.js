0cec6ae16ef786ebe0e81faa31895bca70df11ac
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-15 14:27:35 +0200

    Edit "concat" grunt task to include files in "vendors" folder

--- src/main/tasks/config/concat.js
+++ src/main/tasks/config/concat.js
@@ -3,6 +3,7 @@ module.exports = function (grunt) {
         // Concat all javascript file into `script.js`
         dev:  {
             src:     [
+                'src/main/webapp/resources/_js/vendors/*.js',
                 'src/main/webapp/resources/_js/utils/*.js',
                 'src/main/webapp/resources/_js/components/*.js',
                 'src/main/webapp/resources/_js/beans/*.js'
