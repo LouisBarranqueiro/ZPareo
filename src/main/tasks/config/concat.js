ab4673fba9d4c19fde414bb6ab527a58e4fa99b2
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-04-15 17:38:25 +0200

    Add "animations" folder to "concat" grunt task

--- src/main/tasks/config/concat.js
+++ src/main/tasks/config/concat.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
                 'src/main/webapp/resources/_js/vendors/*.js',
                 'src/main/webapp/resources/_js/utils/*.js',
                 'src/main/webapp/resources/_js/components/*.js',
+                'src/main/webapp/resources/_js/animations/*.js',
                 'src/main/webapp/resources/_js/beans/*.js'
             ],
             dest:    'src/main/webapp/resources/js/script.js',
